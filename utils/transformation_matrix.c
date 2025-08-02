bbe930fe28211b389958acf068687e0e9a2dc20f
Razan Srour <razanmsrour@gmail.com>
2025-08-03 01:05:23 +0300

    finally fixed rotation matrix

--- utils/transformation_matrix.c
+++ utils/transformation_matrix.c
@@ -6,7 +6,7 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 02:01:34 by rsrour            #+#    #+#             */
-/*   Updated: 2025/08/01 10:53:33 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/08/03 01:03:20 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,22 +70,12 @@ The transformation matrix is built by applying scale, then rotation, and finally
 	| 0   0   0   1  |
 */
 
-t_mat4  *ft_get_scale_mat(t_object *obj)
+void  ft_get_scale_mat(t_object *obj, t_mat4 *scale_mat)
 {
-	t_mat4 *scale_mat;
-	
-	scale_mat = (t_mat4 *)malloc(sizeof(t_mat4));
-	if (!scale_mat)
-	{
-		//log_error("Failed to allocate memory for scale matrix");
-		return (NULL);
-	}
-	ft_bzero(scale_mat, sizeof(t_mat4));
 	scale_mat->m[0][0] = obj->transform.scale.x;
 	scale_mat->m[1][1] = obj->transform.scale.y;
 	scale_mat->m[2][2] = obj->transform.scale.z;
 	scale_mat->m[3][3] = 1.0f;
-	return (scale_mat);
 }
 
 static void ft_init_rcx(t_rcx *rcx, t_object *obj)
@@ -103,40 +93,40 @@ static void ft_init_rcx(t_rcx *rcx, t_object *obj)
 
 //I will be using rcx struct to hold the rotation angles and sine and cosine values
 // to minimize computations and for norme purposes
-t_mat4 	*ft_get_rotation_mat(t_object *obj)
+void 	ft_get_rotation_mat(t_object *obj, t_mat4 *rotation_mat)
 {
-	t_mat4 *rotation_mat;
 	t_rcx rcx;
 	
-	rotation_mat = (t_mat4 *)malloc(sizeof(t_mat4));
-	if (!rotation_mat)
-	{
-		//log_error("Failed to allocate memory for rotation matrix");
-		return (NULL);
-	}
-	ft_bzero(rotation_mat, sizeof(t_mat4));
 	ft_init_rcx(&rcx, obj);
 	rotation_mat->m[0][0] = rcx.cy * rcx.cz;
-	rotation_mat->m[0][1] = rcx.cy * rcx.sz;
-	rotation_mat->m[0][2] = -rcx.sy;
+	rotation_mat->m[0][1] = rcx.cz * rcx.sy * rcx.sx - rcx.sz * rcx.cx;
+	rotation_mat->m[0][2] = rcx.cz * rcx.sy * rcx.cx + rcx.sz * rcx.sx;
 	rotation_mat->m[0][3] = 0.0f;
-	rotation_mat->m[1][0] = -rcx.sz * rcx.cx + rcx.cy * rcx.sz; //(incorrect step) to be continued
-	return (rotation_mat);
+	rotation_mat->m[1][0] = rcx.sz * rcx.cy;
+	rotation_mat->m[1][1] = rcx.sz * rcx.sy * rcx.sx + rcx.cz * rcx.cx;
+	rotation_mat->m[1][2] = rcx.sz * rcx.sy * rcx.cx - rcx.cz * rcx.sx;
+	rotation_mat->m[1][3] = 0.0f;
+	rotation_mat->m[2][0] = -rcx.sy;
+	rotation_mat->m[2][1] = rcx.cy * rcx.sx;
+	rotation_mat->m[2][2] = rcx.cy * rcx.cx;
+	rotation_mat->m[2][3] = 0.0f;
+	rotation_mat->m[3][0] = 0.0f;
+	rotation_mat->m[3][1] = 0.0f;
+	rotation_mat->m[3][2] = 0.0f;
+	rotation_mat->m[3][3] = 1.0f;
 }
 
-int	get_transform_matrix(void *_obj, t_mat4 *mat)
+void	get_transform_matrix(void *_obj, t_mat4 *mat)
 {
     t_object *obj;
-	t_mat4 *scale_mat;
+	t_mat4 scale_mat;
+	t_mat4 rotation_mat;
 
     obj = _obj;
-    ft_bzero(mat, sizeof(t_mat4));
-    scale_mat = ft_get_scale_mat(obj);
-	if (!scale_mat)
-	{
-		//log_error("Failed to get scale matrix");
-		return (-1);
-	}
+	ft_memset(&scale_mat, 0, sizeof(t_mat4));
+	ft_memset(&rotation_mat, 0, sizeof(t_mat4));
+    ft_get_scale_mat(obj, &scale_mat);
+	ft_get_rotation_mat(obj, &rotation_mat);
     mat->m[3][0] = obj->transform.position.x; // Set translation in x
     mat->m[3][1] = obj->transform.position.y; // Set translation in y
     mat->m[3][2] = obj->transform.position.z; // Set translation in z
@@ -145,5 +135,4 @@ int	get_transform_matrix(void *_obj, t_mat4 *mat)
     mat->m[1][3] = 0.0f; // Set homogeneous coordinate for y
     mat->m[2][3] = 0.0f; // Set homogeneous coordinate for z
     //set rotation matrix based on obj->transform.rotation
-	return (0);
 }
